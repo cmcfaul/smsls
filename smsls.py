297a297a3e31023c2f024053541014341327c497
cmcfaul <cmcfaul@b3ed1254-2567-49ed-a220-ea4c30f9f803>
2013-05-15 12:46:17 +0000

    First working draft of python version. Successfully opens a temperature and ls file, and prints out some of the ls data.
    git-svn-id: svn+ssh://zimm/usr/local/svn/repos/smsls@4 b3ed1254-2567-49ed-a220-ea4c30f9f803

--- smsls.py
+++ smsls.py
@@ -2,6 +2,7 @@
 #$Id$
 from numpy import *
 
+datadir = 'C:\\Users\\cmcfaul\\Documents\\Data\\2013-03-25\\'
 #setup the array to hold the pixels we want to keep
 step = 3
 cell_center = array ([20, 89, 155, 221, 285, 351, 418, 486])
@@ -9,26 +10,26 @@ range = step*arange(-5, 6, 1)
 rangex,cellx = ix_(range, cell_center)
 pixels = rangex+cellx
 
-tempfile = open('c121031_stab_rev_temperatures.txt')
+tempfile = open(datadir + 'c121031_stab_rev_temperatures.txt')
 tempfile.readline() #header size
 tempfile.readline() #version
 header = tempfile.readline() #sampling interval
-interval = int(header.split(':')[1]
+interval = int(header.split(':')[1])
 tempfile.readline() #sampling rate
 tempfile.readline() #sample count
 tempfile.readline() #device serial number
 tempfile.readline() #column titles
 
-datafile = open('c121031_stab_rev_01.txt')
+datafile = open(datadir + 'c121031_stab_rev_01.txt')
 datafile.readline() #sentinal line
 datafile.readline() #column titles
 
 temp = tempfile.readline()
 data = datafile.readline()
 ls = data.split('\t')
-for idx in pixels[:,0]
-	print ls[idx]
-	print ',' 
+for idx in pixels[:,0]:
+	print repr(idx) + ', ' + repr(ls[idx]) + ',' 
 print '\n'
 
-f.close
+tempfile.close
+datafile.close

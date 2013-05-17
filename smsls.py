86ea3613c13864affc0b43b1a5646c23d7b5b7fc
cmcfaul <cmcfaul@b3ed1254-2567-49ed-a220-ea4c30f9f803>
2013-05-17 21:53:54 +0000

    Correctly reads in exactly one ls data file and the corresponding lines from the temperature file. Prints out the time, the temperature, and the ls data for cell 1 correctly. Does not handle cells 2 thouugh 8 correctly.
    git-svn-id: svn+ssh://zimm/usr/local/svn/repos/smsls@5 b3ed1254-2567-49ed-a220-ea4c30f9f803

--- smsls.py
+++ smsls.py
@@ -3,6 +3,7 @@
 from numpy import *
 
 datadir = 'C:\\Users\\cmcfaul\\Documents\\Data\\2013-03-25\\'
+basename = 'c121031_stab_rev'
 #setup the array to hold the pixels we want to keep
 step = 3
 cell_center = array ([20, 89, 155, 221, 285, 351, 418, 486])
@@ -10,7 +11,7 @@ range = step*arange(-5, 6, 1)
 rangex,cellx = ix_(range, cell_center)
 pixels = rangex+cellx
 
-tempfile = open(datadir + 'c121031_stab_rev_temperatures.txt')
+tempfile = open(datadir + basename + '_temperatures.txt')
 tempfile.readline() #header size
 tempfile.readline() #version
 header = tempfile.readline() #sampling interval
@@ -20,16 +21,29 @@ tempfile.readline() #sample count
 tempfile.readline() #device serial number
 tempfile.readline() #column titles
 
-datafile = open(datadir + 'c121031_stab_rev_01.txt')
+datafile = open(datadir + basename + '_01.txt')
 datafile.readline() #sentinal line
-datafile.readline() #column titles
 
-temp = tempfile.readline()
-data = datafile.readline()
-ls = data.split('\t')
-for idx in pixels[:,0]:
-	print repr(idx) + ', ' + repr(ls[idx]) + ',' 
-print '\n'
-
+header = datafile.readline() #column titles
+column = header.split('\t')
+for i in arange(8):
+	g = open(datadir + basename + '_c0' + str(i+1) + '.txt', 'w')
+	g.write('Time (s), Temperature (C), ' , )
+	for idx in pixels[:,0]:
+		g.write( str(column[idx+1]) + ', ', )
+	g.write('\n')
+	for line in iter(datafile):
+		#data = datafile.readline()
+		ls = line.split('\t')
+		data = tempfile.readline()
+		temp = data.split(',')
+		g.write(str(ls[0]) + ', ' , )
+		g.write(str(temp[i+2]) + ', ' , )
+		for idx in pixels[:,0]:
+			g.write(str(ls[idx+1]) + ', ' , )
+		g.write('\n')
+	g.close
+	print 'file ' + str(i) + ' is done.'
+	
 tempfile.close
 datafile.close

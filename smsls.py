5566d7ad8df62201bd73149872eea59222743439
cmcfaul <cmcfaul@b3ed1254-2567-49ed-a220-ea4c30f9f803>
2013-05-18 18:00:15 +0000

    Now works correctly for one imput ls and one input temperature file. Outputs 8 files, one for each sample cell. File names are still hard-coded.
    git-svn-id: svn+ssh://zimm/usr/local/svn/repos/smsls@6 b3ed1254-2567-49ed-a220-ea4c30f9f803

--- smsls.py
+++ smsls.py
@@ -4,6 +4,11 @@ from numpy import *
 
 datadir = 'C:\\Users\\cmcfaul\\Documents\\Data\\2013-03-25\\'
 basename = 'c121031_stab_rev'
+outFile = []
+for i in arange(8):
+	f = open(datadir + basename + '_c0' + str(i+1) + '.dat', 'w')
+	outFile.append(f)
+
 #setup the array to hold the pixels we want to keep
 step = 3
 cell_center = array ([20, 89, 155, 221, 285, 351, 418, 486])
@@ -27,23 +32,23 @@ datafile.readline() #sentinal line
 header = datafile.readline() #column titles
 column = header.split('\t')
 for i in arange(8):
-	g = open(datadir + basename + '_c0' + str(i+1) + '.txt', 'w')
-	g.write('Time (s), Temperature (C), ' , )
-	for idx in pixels[:,0]:
-		g.write( str(column[idx+1]) + ', ', )
-	g.write('\n')
-	for line in iter(datafile):
-		#data = datafile.readline()
-		ls = line.split('\t')
-		data = tempfile.readline()
-		temp = data.split(',')
-		g.write(str(ls[0]) + ', ' , )
-		g.write(str(temp[i+2]) + ', ' , )
-		for idx in pixels[:,0]:
-			g.write(str(ls[idx+1]) + ', ' , )
-		g.write('\n')
-	g.close
-	print 'file ' + str(i) + ' is done.'
+	outFile[i].write('Time (s), Temperature (C), ' , )
+	for idx in pixels[:,i]:
+		outFile[i].write( str(column[idx+1]) + ', ', )
+	outFile[i].write('\n')
+for line in iter(datafile):
+	#data = datafile.readline()
+	ls = line.split('\t')
+	data = tempfile.readline()
+	temp = data.split(',')
+	for i in arange(8):
+		outFile[i].write(str(ls[0]) + ', ' , )
+		outFile[i].write(str(temp[i+2]) + ', ' , )
+		for idx in pixels[:,i]:
+			outFile[i].write(str(ls[idx+1]) + ', ' , )
+		outFile[i].write('\n')
+for i in arange(8):
+	outFile[i].close()
 	
-tempfile.close
-datafile.close
+tempfile.close()
+datafile.close()

e9971f0223c38a7619dc0a3f93660ca51f7e17ed
cmcfaul <cmcfaul@b3ed1254-2567-49ed-a220-ea4c30f9f803>
2013-05-19 19:52:11 +0000

    Get input data from user input
    git-svn-id: svn+ssh://zimm/usr/local/svn/repos/smsls@7 b3ed1254-2567-49ed-a220-ea4c30f9f803

--- smsls.py
+++ smsls.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python
 #$Id$
-from numpy import *
 
-datadir = 'C:\\Users\\cmcfaul\\Documents\\Data\\2013-03-25\\'
-basename = 'c121031_stab_rev'
+datadir = 'C:\\Users\\cmcfaul\\Documents\\Data\\'
+datedir = raw_input('Enter the date of the data (in yyyy-mm-dd) ')
+basename = raw_input('What is the basename of the file? ') #'c121031_stab_rev'
 outFile = []
 for i in arange(8):
-	f = open(datadir + basename + '_c0' + str(i+1) + '.dat', 'w')
+	f = open(datadir + datedir + '\\' + basename + '_c0' + str(i+1) + '.dat', 'w')
 	outFile.append(f)
 
 #setup the array to hold the pixels we want to keep
 step = 3
 cell_center = array ([20, 89, 155, 221, 285, 351, 418, 486])
-range = step*arange(-5, 6, 1)
-rangex,cellx = ix_(range, cell_center)
+cell_range = step*arange(-5, 6, 1)
+rangex,cellx = ix_(cell_range, cell_center)
 pixels = rangex+cellx
 
-tempfile = open(datadir + basename + '_temperatures.txt')
+tempfile = open(datadir + datedir + '\\' + basename + '_temperatures.txt')
 tempfile.readline() #header size
 tempfile.readline() #version
 header = tempfile.readline() #sampling interval
@@ -26,7 +26,7 @@ tempfile.readline() #sample count
 tempfile.readline() #device serial number
 tempfile.readline() #column titles
 
-datafile = open(datadir + basename + '_01.txt')
+datafile = open(datadir + datedir + '\\' + basename + '_01.txt')
 datafile.readline() #sentinal line
 
 header = datafile.readline() #column titles
@@ -37,7 +37,6 @@ for i in arange(8):
 		outFile[i].write( str(column[idx+1]) + ', ', )
 	outFile[i].write('\n')
 for line in iter(datafile):
-	#data = datafile.readline()
 	ls = line.split('\t')
 	data = tempfile.readline()
 	temp = data.split(',')
